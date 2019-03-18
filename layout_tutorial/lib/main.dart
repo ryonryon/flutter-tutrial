6889d9855cc9d7fc330f25cfbafa6ac64ee647e8
ryotogashi <ryo.togashi.ca@gmail.com>
2019-03-18 13:41:28 -0700

    Add layout tutorial

--- layout_tutorial/lib/main.dart
+++ layout_tutorial/lib/main.dart
@@ -1,124 +1,198 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
-  final kichen = Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(Icons.kitchen, color: Colors.green[500]),
-        Text('PREP:'),
-        Text('25 min'),
-      ],
-    )
-  );
-
-  final restaurant = Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(Icons.restaurant, color: Colors.green[500]),
-        Text('FEEDS:'),
-        Text('4-6'),
-      ],
-    )
-  );
-
-  final timer = Center(
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Icon(Icons.timer, color: Colors.green[500]),
-        Text('COOK:'),
-        Text('1 hr'),
-      ],
-    )
-  );
-
-  final padding = Container(
-    decoration: BoxDecoration(
-      color: Colors.black26
-    ),
-    child: Row(
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.all(50.0),
-          child: Text('Paddint test!'),
-        ),
-
-        Padding(
-          padding: EdgeInsets.all(50.0),
-          child: Text('Padding test2!')
-        )
-      ],
-    ),
-  );
-  
-  // Widget _buildDecoratedImage() => Expanded(
-  //     child: Container(
-  //       decoration: BoxDecoration(
-  //         border: Border.all(width: 10, color: Colors.black38),
-  //         borderRadius: const BorderRadius.all(const Radius.circular(8)),
-  //       ),
-  //       margin: const EdgeInsets.all(4),
-  //       child: Image.asset('assets/lgtm1.jpg'),
-  //     ),
-  //   );
-
-  Widget _listViews() => ListView(
-    children: <Widget>[
-      _tile('title', Icons.theaters),
-      _tile('title', Icons.theaters),
-      _tile('title', Icons.theaters),
-      _tile('title', Icons.theaters),
-      _tile('title', Icons.theaters),
-      _tile('title', Icons.theaters),
-      _tile('title', Icons.theaters),
-      _tile('title', Icons.theaters),
-    ],
-  );
-
-  ListTile _tile(String title, IconData icon) => ListTile(
-    title: Text(title,
-      style: TextStyle(
-        fontWeight: FontWeight.w500,
-        fontSize: 20
-      ),
-    ),
-    leading: Icon(
-      icon,
-      color: Colors.blue[500],
-    ),
-  );
 
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Oeschinen Lake Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Kandersteg, Switzerland',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          FavoriteWidget(),
+        ],
+      ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
     return MaterialApp(
-      title: 'Layout tutorial',
+      title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter layout'),
+          title: Text('Flutter layout demo'),
         ),
-        body: _listViews(),
-
-        // body: Column(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: <Widget>[
-        //     Row(
-        //       mainAxisAlignment: MainAxisAlignment.center,
-        //       children: <Widget>[
-        //         kichen,
-        //         timer,
-        //         restaurant,
-        //       ],
-        //     ),
-        //   ],
-        // )
-      )
+        body: ListView(
+          children: [
+            Image.asset(
+              'assets/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
+        ),
+      ),
     );
   }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget{
+
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child:IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+
+        SizedBox(
+          width: 18.0,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          )
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if(_isFavorited) {
+        _isFavorited = false;
+        _favoriteCount -= 1;
+      } else {
+        _isFavorited = true;
+        _favoriteCount += 1;
+      }
+    });
+  }
+}
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        child: Center(
+          child: Text(
+            _active ? 'Active' : 'Inactive',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: _active ? Colors.green[700] : Colors.grey[600]
+        ),
+      ),
+    );
+  }
+
 }
